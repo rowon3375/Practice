bc7aceba086de01bda27e1cb9fa47e0ea76abdcc
hwawon <ilove4622@gmail.com>
2022-09-15 20:14:55 +0900

    9/9(金)　HTML practic 1. 1. IU profile page start
    9/15(木)　JavaScript practic
    1. JavaScript basic practic
    2. JavaScript study practic
    3. PHP study practic

---
+++ study_실습/PHP/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>JavaScript</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+  </head>
+  <body id="target">
+      <header>
+        <img src="https://s3-ap-northeast-2.amazonaws.com/opentutorials-user-file/course/1/5641.png" alt="손">
+        <h1><a href="http://localhost:8080/php.php">JavaScript</a></h1>
+      </header>
+      <nav>
+        <ol>
+          <?php
+            echo file_get_contents("list.txt");
+           ?>
+        </ol>
+      </nav>
+
+      <div id="control">
+        <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
+        <input type="button" value="black" onclick="document.getElementById('target').className='black'"/>
+      </div>
+
+      <article>
+        <?php
+        if(empty($_GET['id']) == false){
+          echo file_get_contents($_GET['id'].".txt");
+        }
+
+         ?>
+      </article>
+  </body>
+</html>
